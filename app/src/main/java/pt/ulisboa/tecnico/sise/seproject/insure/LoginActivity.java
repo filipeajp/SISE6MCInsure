5f3bddc00f744ea78ced6dd7fb1a56e1b837264e
Luís Rodrigues <luis.cnm@gmail.com>
2020-03-26 16:34:04 +0000

    added activity flow to login & main page activities' buttons

--- app/src/main/java/pt/ulisboa/tecnico/sise/seproject/insure/LoginActivity.java
+++ app/src/main/java/pt/ulisboa/tecnico/sise/seproject/insure/LoginActivity.java
@@ -2,13 +2,30 @@ package pt.ulisboa.tecnico.sise.seproject.insure;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button buttonLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        buttonLogin = findViewById(R.id.login_button);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, MainPageActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
--- app/src/main/java/pt/ulisboa/tecnico/sise/seproject/insure/MainPageActivity.java
+++ app/src/main/java/pt/ulisboa/tecnico/sise/seproject/insure/MainPageActivity.java
@@ -2,13 +2,67 @@ package pt.ulisboa.tecnico.sise.seproject.insure;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainPageActivity extends AppCompatActivity {
+
+    private Button buttonLogout;
+
+    private Button buttonMyInformation;
+    private Button buttonNewClaim;
+    private Button buttonMyClaims;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+
+        buttonLogout = findViewById(R.id.main_page_logout_button);
+
+        buttonMyInformation = findViewById(R.id.main_page_my_information_button);
+        buttonNewClaim = findViewById(R.id.main_page_new_claim_button);
+        buttonMyClaims = findViewById(R.id.main_page_my_claims_button);
+
+        buttonLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainPageActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        buttonMyInformation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainPageActivity.this, MyInformationActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        buttonNewClaim.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainPageActivity.this, NewClaimActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        buttonMyClaims.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainPageActivity.this, MyClaimsActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
+
+
 }
