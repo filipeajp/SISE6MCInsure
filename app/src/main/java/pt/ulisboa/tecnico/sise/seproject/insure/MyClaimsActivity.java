f91e04268c46898b448540835e7ec8587cd0b549
Susanadsf <susanadsfranco@gmail.com>
2020-03-26 12:30:35 +0000

    Changes: Added MyClaimsActivity and ReadClaimActivity

---
+++ app/src/main/java/pt/ulisboa/tecnico/sise/seproject/insure/MyClaimsActivity.java
@@ -0,0 +1,14 @@
+package pt.ulisboa.tecnico.sise.seproject.insure;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyClaimsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_claims);
+    }
+}
---
+++ app/src/main/java/pt/ulisboa/tecnico/sise/seproject/insure/ReadClaimActivity.java
@@ -0,0 +1,14 @@
+package pt.ulisboa.tecnico.sise.seproject.insure;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReadClaimActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_read_claim);
+    }
+}
