af0ffa8a6118535a2699412538ebb5b122c1a070
Luís Rodrigues <luis.cnm@gmail.com>
2020-04-07 17:46:34 +0100

    added datepicker to new claim

--- app/src/main/java/pt/ulisboa/tecnico/sise/seproject/insure/activities/NewClaimActivity.java
+++ app/src/main/java/pt/ulisboa/tecnico/sise/seproject/insure/activities/NewClaimActivity.java
@@ -1,19 +1,24 @@
 package pt.ulisboa.tecnico.sise.seproject.insure.activities;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import pt.ulisboa.tecnico.sise.seproject.insure.GlobalState;
@@ -22,13 +27,13 @@ import pt.ulisboa.tecnico.sise.seproject.insure.wscalltasks.GetPlatesTask;
 import pt.ulisboa.tecnico.sise.seproject.insure.wscalltasks.NewClaimTask;
 import pt.ulisboa.tecnico.sise.seproject.insure.wscalltasks.TaskCallBack;
 
-public class NewClaimActivity extends AppCompatActivity implements TaskCallBack, AdapterView.OnItemSelectedListener {
+public class NewClaimActivity extends AppCompatActivity implements TaskCallBack, AdapterView.OnItemSelectedListener, DatePickerDialog.OnDateSetListener {
     private static final String LOG_TAG = "SISE - NewClaim";
     private GlobalState _globalState;
     private Button buttonSubmit;
     private Button buttonCancel;
     private EditText editTextTitle;
-    private EditText editTextOccurrenceDate;
+    private TextView viewOccurrenceDate;
     private EditText editTextDesc;
     private Spinner plateSpinner;
 
@@ -43,29 +48,36 @@ public class NewClaimActivity extends AppCompatActivity implements TaskCallBack,
         buttonSubmit = (Button) findViewById(R.id.new_claim_btn_submit);
         buttonCancel = (Button) findViewById(R.id.new_claim_btn_cancel);
         editTextTitle = (EditText) findViewById(R.id.new_claim_claim_title);
-        editTextOccurrenceDate = (EditText) findViewById(R.id.new_claim_occurrence_date);
+        viewOccurrenceDate = (TextView) findViewById(R.id.new_claim_occurrence_date);
         editTextDesc = (EditText) findViewById(R.id.new_claim_description);
         plateSpinner = (Spinner) findViewById(R.id.new_claim_plate_spinner);
 
         plateSpinner.setOnItemSelectedListener(this);
         plateSpinner.setSelection(0);
 
+        viewOccurrenceDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //opens datePicker and sets text equal to the selected date (string format)
+                showDatePickerDialog();
+            }
+        });
+
         buttonSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Log.d(LOG_TAG, editTextTitle.getText().toString());
                 Log.d(LOG_TAG, plateSpinner.getSelectedItem().toString());
-                Log.d(LOG_TAG, editTextOccurrenceDate.getText().toString());
+                Log.d(LOG_TAG, viewOccurrenceDate.getText().toString());
                 Log.d(LOG_TAG, editTextDesc.getText().toString());
                 String claimTitle = editTextTitle.getText().toString();
                 String claimPlateNumber = plateSpinner.getSelectedItem().toString();
-                String claimOccurDate = editTextOccurrenceDate.getText().toString();
+                String claimOccurDate = viewOccurrenceDate.getText().toString();
                 String claimDesc = editTextDesc.getText().toString();
 
                 new NewClaimTask(view.getContext(), _globalState.getSessionId(), claimTitle, claimOccurDate, claimPlateNumber, claimDesc).execute();
             }
         });
-
 
         buttonCancel.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -75,6 +87,32 @@ public class NewClaimActivity extends AppCompatActivity implements TaskCallBack,
                 finish();
             }
         });
+    }
+
+    private void showDatePickerDialog() {
+        DatePickerDialog datePicker = new DatePickerDialog(
+                this,
+                this,
+                Calendar.getInstance().get(Calendar.YEAR),
+                Calendar.getInstance().get(Calendar.MONTH),
+                Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+        );
+        datePicker.getDatePicker().setMaxDate(new Date().getTime());
+        datePicker.show();
+    }
+
+    @Override
+    public void onDateSet(DatePicker dp, int y, int m, int d) {
+        String selectedDate = addZero(d) + "-" + addZero(m + 1) + "-" + y;
+        viewOccurrenceDate.setText(selectedDate);
+    }
+
+    private String addZero(int n) {
+        if(n < 10) {
+            return "0" + n;
+        } else {
+            return String.valueOf(n);
+        }
     }
 
     @Override
